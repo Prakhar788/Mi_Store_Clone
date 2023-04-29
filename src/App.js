dfd953e93c74742a09df7e12242eb3b9167e2718
Prakhar Sharma <prakhar.sharma_cs20@gla.ac.in>
2023-04-30 02:18:09 +0530

    Adding New Flies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import PreNavbar from "./components/PreNavbar";
-
+import Navbar from "./components/Navbar";
+import {  BrowserRouter as Router,Route } from "react-router-dom"
 function App() {
   return (
-   <>
+   <Router>
    <PreNavbar/>
-   </>
+   <Navbar/>
+   </Router>
   );
 }
 
